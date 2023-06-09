ef16916742779b5a14a8dbd9e156202a8c861d9b
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-09 13:11:14 +0530

    [create table and their columns]

--- ProgramSQLQuery1.sql
+++ ProgramSQLQuery1.sql
@@ -1,3 +1,18 @@
+/*UC-1
+
 CREATE DATABASE payroll_service;
 USE payroll_service;
 SHOW DATABASE;
+------------
+*/
+
+Create Table employeePayroll
+(
+ID int identity(1,1) primary key,
+NAME varchar (100),
+SALARY Decimal(10,2),
+Date Date
+);
+
+
+
