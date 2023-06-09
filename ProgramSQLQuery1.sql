41b1fc09a94de4cad44367f6130541eaa373a773
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-09 13:36:46 +0530

    [insert data in table]

--- ProgramSQLQuery1.sql
+++ ProgramSQLQuery1.sql
@@ -4,7 +4,8 @@ CREATE DATABASE payroll_service;
 USE payroll_service;
 SHOW DATABASE;
 ------------
-*/
+
+UC-2
 
 Create Table employeePayroll
 (
@@ -13,6 +14,23 @@ NAME varchar (100),
 SALARY Decimal(10,2),
 Date Date
 );
+*/
+
+
+
+
+--UC-3
+
+INSERT INTO  employeePayroll
+(NAME,SALARY,Date)
+Values
+('Ram', '10000.50', '2023-05-15'),
+('Shyam', '15000.00', '2023-01-20'),
+('Mohan', '13000.21', '2023-03-12'),
+('Ramesh', '17000.34', '2022-07-07'),
+('Ramesh', '17000.01', '2022-11-01');
+
+select * from employeePayroll;
 
 
 
